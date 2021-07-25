9e05e791a8d9e0af8c3b82261025d3302f0d4596
Soukaigiwar <54086765+Soukaigiwar@users.noreply.github.com>
2021-07-24 22:28:49 -0300

    Create create_ssh_keys_42.sh

---
+++ create_ssh_keys_42.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "SSH file name?"
+echo ""
+read SSH_NAME
+echo ""
+echo "What is your email?"
+echo ""
+read EMAIL
+echo ""
+echo "These files will be created in ~/.ssh/ folder: [$SSH_NAME] and [$SSH_NAME.pub]."
+echo "With this email address: $EMAIL ."
+echo ""
+ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/$SSH_NAME -C "$EMAIL" && 
+eval "$(ssh-agent -s)" &&
+echo "ssh-add ~/.ssh/$SSH_NAME &&"
+echo "==========================================================" &&
+echo "=----COPY THIS PUBLIC KEY AND PASTE INTO THE 42 INTRA----=" &&
+echo "==========================================================" &&
+echo ""
+cat ~/.ssh/"$SSH_NAME".pub
+echo ""
